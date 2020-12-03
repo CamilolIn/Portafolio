234d1f89bf32aa966c7d4feb26316801b26dad27
camilolin <camilolindarte1992@gmail.com>
2020-12-02 21:57:29 -0500

    Banner whit background and my name

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from '../styles/banner.module.css'
+
+const Banner = () => {
+    return (
+        <div className={s.cont__banner}>
+        <h1>I am the Banner</h1>
+        </div>
+    )
+}
+
+export default Banner   
